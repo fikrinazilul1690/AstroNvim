10518d48375c2c4d350d1af9e94d92ad111d2118
fikrinazilul1690 <fikrinazilul@gmail.com>
2024-06-01 20:17:51 +0700

    overrides neogen config

---
+++ lua/plugins/neogen.lua
@@ -0,0 +1,12 @@
+return {
+  "danymat/neogen",
+  opts = {
+    languages = {
+      lua = { template = { annotation_convention = "ldoc" } },
+      typescript = { template = { annotation_convention = "tsdoc" } },
+      typescriptreact = { template = { annotation_convention = "tsdoc" } },
+      javascript = { template = { annotation_convention = "jsdoc" } },
+      javascriptreact = { template = { annotation_convention = "jsdoc" } },
+    },
+  },
+}
