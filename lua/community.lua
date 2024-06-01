2d1c94a3b5dc0be60a047f75768d63046b2d6456
fikrinazilul1690 <fikrinazilul@gmail.com>
2024-06-01 22:44:42 +0700

    move astrocommunity config

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -7,6 +7,21 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.prisma" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.editing-support.neogen" },
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.templ" },
+  { import = "astrocommunity.pack.proto" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.editing-support.treesj" },
+  { import = "astrocommunity.completion.cmp-cmdline" },
+  { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.editing-support.conform-nvim" },
+  { import = "astrocommunity.game.leetcode-nvim" },
 }
