1e072a63e75f684c20f55aaae48879fe56bd0fec
fikrinazilul1690 <fikrinazilul@gmail.com>
2024-06-07 14:33:41 +0700

    add custom configuration for ts

--- lua/community.lua
+++ lua/community.lua
@@ -13,6 +13,7 @@ return {
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.full-dadbod" },
+  { import = "astrocommunity.pack.php" },
   { import = "astrocommunity.pack.prisma" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
   { import = "astrocommunity.editing-support.neogen" },
