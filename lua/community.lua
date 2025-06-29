24395896faa074d85b6b910b3627f28d52a094b2
Achmad Nazilul Fikri <fikrinazilul@gmail.com>
2025-06-29 20:47:45 +0700

    update

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,4 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if false then return {} end -- WARN: UNCOMMENT THIS LINE TO DEACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -9,12 +9,12 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.pack.go" },
-  { import = "astrocommunity.pack.java" },
+  -- { import = "astrocommunity.pack.java" },
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.full-dadbod" },
-  { import = "astrocommunity.pack.php" },
+  -- { import = "astrocommunity.pack.php" },
   { import = "astrocommunity.pack.prisma" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
   { import = "astrocommunity.pack.python" },
@@ -24,8 +24,10 @@ return {
   { import = "astrocommunity.pack.proto" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.editing-support.treesj" },
+  { import = "astrocommunity.editing-support.mcphub-nvim" },
   { import = "astrocommunity.completion.blink-cmp" },
-  -- { import = "astrocommunity.completion.codeium-vim" },
+  -- { import = "astrocommunity.completion.nvim-cmp" },
+  { import = "astrocommunity.completion.avante-nvim" },
   { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.game.leetcode-nvim" },
   { import = "astrocommunity.motion.harpoon" },
